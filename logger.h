c01ec2df5d110c6944285cd4fb43f85790a1f859
Jacky Li <drsl@drshapeless.com>
2024-10-03 21:13:56 +0800

    Rename all log functions

--- logger.h
+++ logger.h
@@ -27,37 +27,39 @@ void _log_output(enum LogLevel level, const char *format, ...);
 #define LOG_VERBOSE_ENABLED 1
 #endif
 
-#define FATAL(message, ...)                               \
+#define log_fatal(message, ...)                           \
     _log_output(LOG_LEVEL_FATAL, message, ##__VA_ARGS__); \
     debug_break();
 
-#ifndef ERROR
-#define ERROR(message, ...) _log_output(LOG_LEVEL_ERROR, message, ##__VA_ARGS__)
-#endif
+#define log_error(message, ...) \
+    _log_output(LOG_LEVEL_ERROR, message, ##__VA_ARGS__)
 
 #if LOG_WARN_ENABLED == 1
-#define WARN(message, ...) _log_output(LOG_LEVEL_WARN, message, ##__VA_ARGS__)
+#define log_warn(message, ...) \
+    _log_output(LOG_LEVEL_WARN, message, ##__VA_ARGS__)
 #else
-#define WARN(message, ...)
+#define log_warn(message, ...)
 #endif
 
 #if LOG_INFO_ENABLED == 1
-#define INFO(message, ...) _log_output(LOG_LEVEL_INFO, message, ##__VA_ARGS__)
+#define log_info(message, ...) \
+    _log_output(LOG_LEVEL_INFO, message, ##__VA_ARGS__)
 #else
-#define INFO(message, ...)
+#define log_info(message, ...)
 #endif
 
 #if LOG_DEBUG_ENABLED == 1
-#define DEBUG(message, ...) _log_output(LOG_LEVEL_DEBUG, message, ##__VA_ARGS__)
+#define log_debug(message, ...) \
+    _log_output(LOG_LEVEL_DEBUG, message, ##__VA_ARGS__)
 #else
-#define DEBUG(message, ...)
+#define log_debug(message, ...)
 #endif
 
 #if LOG_VERBOSE_ENABLED == 1
-#define VERBOSE(message, ...) \
+#define log_verbose(message, ...) \
     _log_output(LOG_LEVEL_VERBOSE, message, ##__VA_ARGS__)
 #else
-#define VERBOSE(message, ...)
+#define log_verbose(message, ...)
 #endif
 
 #endif /* LOGGER_H */
